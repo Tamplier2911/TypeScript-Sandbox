46b59e80b6a50aa9a410550f65b6b1068505ac4a
Tamplier2911 <44438728+Tamplier2911@users.noreply.github.com>
2020-02-20 21:09:30 +0200

    Trying TS with react classes and functional components TSX rules! :)

--- 008_React_With_TS/reacttypescript/src/App.tsx
+++ 008_React_With_TS/reacttypescript/src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
+interface AppProps {
+  color?: string;
+}
+
+const App = (props: AppProps) => {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello TSX</h1>
+      <p>App Component color is: {props.color}</p>
+      <ClassApp color="blue" />
     </div>
   );
+};
+
+interface CLassAppState {
+  count: number;
+}
+
+class ClassApp extends Component<AppProps, CLassAppState> {
+  /*
+  public state: CLassAppState;
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = {
+      count: 0
+    };
+  }
+  */
+
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  onIncrementCounter() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  onDecrementCounter() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <div>Class component color is: {this.props.color}</div>
+        <div className="counter">
+          <button
+            type="button"
+            className="counter__button"
+            onClick={() => this.onDecrementCounter()}
+          >
+            -
+          </button>
+          <div className="counter__value">{this.state.count}</div>
+          <button
+            type="button"
+            className="counter__button"
+            onClick={() => this.onIncrementCounter()}
+          >
+            +
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
